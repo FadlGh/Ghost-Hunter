5369cbc78ad4f8bd1e0bb23b63bc87584bc7af71
unknown <fadl2009gh@gmail.com>
2023-05-08 20:10:35 +0300

    Added door and key mechanics + art #2

---
+++ Assets/Scripts/Main/Collectables/Key.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private GameObject _door;
+    [SerializeField] private ParticleSystem _keyPs;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Instantiate(_keyPs, transform.position, Quaternion.identity);
+            Destroy(_door);
+            Destroy(gameObject);
+        }
+    }
+}
